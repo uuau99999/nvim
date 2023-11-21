f32f0ea3749fe2d46c399bddc89ca5288037cabc
hoyup <hoyupye@tencent.com>
2023-11-21 15:40:00 +0800

    chore: update

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,22 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
+keymap.set("n", "x", '"_x')
+
+-- Increment/decrement
+keymap.set("n", "+", "<C-a>")
+keymap.set("n", "-", "<C-x>")
+
+-- Delete a word backwards
+keymap.set("n", "dw", "daw")
+
+-- Select all
+keymap.set("n", "<C-a>", "gg<S-v>G")
+
+-- Split window
+keymap.set("n", "ss", ":split<Return>", opts)
+keymap.set("n", "sv", ":vsplit<Return>", opts)
---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    config = true,
+    keys = {
+      { "<leader>td", "<Cmd>ToggleTerm size=20 dir=$pwd<CR>" },
+    },
+  },
+}
