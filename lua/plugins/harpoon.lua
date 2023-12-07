da1ed25ca92bd45dc06e4f749fcbeee3f81a5e77
uuau99999 <uuau99999@gmail.com>
2023-12-07 16:01:46 +0800

    use harpoon2 and update config

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,20 +1,41 @@
 return {
   "ThePrimeagen/harpoon",
+  branch = "harpoon2",
   lazy = false,
   dependencies = {
     "nvim-lua/plenary.nvim",
   },
   config = function()
-    require("harpoon").setup({
-      menu = {
-        width = 120,
-      },
-    })
+    require("harpoon"):setup({})
   end,
   keys = {
-    { "<leader>m", "<cmd>lua require('harpoon.mark').add_file()<cr>", desc = "Mark file with harpoon" },
-    { "<leader>n", "<cmd>lua require('harpoon.ui').nav_next()<cr>", desc = "Go to next harpoon mark" },
-    { "<leader>p", "<cmd>lua require('harpoon.ui').nav_prev()<cr>", desc = "Go to previous harpoon mark" },
-    { "<leader>a", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", desc = "Show harpoon marks" },
+    {
+      "<leader>m",
+      function()
+        require("harpoon"):list():append()
+      end,
+      desc = "Mark file with harpoon",
+    },
+    {
+      "<leader>n",
+      function()
+        require("harpoon"):list():next()
+      end,
+      desc = "Go to next harpoon mark",
+    },
+    {
+      "<leader>p",
+      function()
+        require("harpoon"):list():prev()
+      end,
+      desc = "Go to previous harpoon mark",
+    },
+    {
+      "<leader>a",
+      function()
+        require("harpoon").ui:toggle_quick_menu(require("harpoon"):list())
+      end,
+      desc = "Show harpoon marks",
+    },
   },
 }
