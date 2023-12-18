bf4210304e0128a431b118e328a1d6445fa8af4c
hoyup <uuau99999@gmail.com>
2023-12-18 23:30:04 +0800

    update telescope config

--- init.lua
+++ init.lua
@@ -5,6 +5,36 @@ require("telescope").load_extension("harpoon")
 require("telescope").load_extension("fzf")
 require("telescope").load_extension("file_browser")
 
+local fzy = require("telescope.algos.fzy")
+local sorters = require("telescope.sorters")
+
+local custom_live_grep_sorter = sorters.Sorter:new({
+  scoring_function = function(_, _, line)
+    if line:find("vue") then
+      return 0
+    elseif line:find("lua") then
+      return 0
+    elseif line:find("ts") then
+      return 1
+    elseif line:find("js") then
+      return 2
+    elseif line:find("json") then
+      return 3
+    elseif line:find("html") then
+      return 4
+    elseif line:find("yml") then
+      return 5
+    elseif line:find("css") then
+      return 6
+    else
+      return 100
+    end
+  end,
+  highlighter = function(_, prompt, display)
+    return fzy.positions(prompt, display)
+  end,
+})
+
 require("telescope").setup({
   defaults = {
     layout_strategy = "horizontal",
@@ -14,6 +44,16 @@ require("telescope").setup({
   },
   pickers = {
     find_files = {
+      theme = "dropdown",
+    },
+    live_grep = {
+      theme = "dropdown",
+      sorter = custom_live_grep_sorter,
+    },
+    git_files = {
+      theme = "dropdown",
+    },
+    oldfiles = {
       theme = "dropdown",
     },
   },
